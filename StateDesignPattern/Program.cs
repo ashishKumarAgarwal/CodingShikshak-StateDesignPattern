2ad50ea647b4c910e0f8cbc5cb6d3e7454bb957f
Ashish Kumar Agarwal <AshishKumar.Agarwal@csgi.com>
2020-09-12 17:33:58 +0530

    Added basic console project

---
+++ StateDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StateDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
